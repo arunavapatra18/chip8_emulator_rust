dad7c74b407c733eec1f43a8960a8e1dcefa1c7d
Arunava Patra <arunavapatra18@gmail.com>
2023-05-18 19:57:16 +0530

    Adding user inputs

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -4,6 +4,7 @@ use sdl2::render::Canvas;
 use sdl2::event::Event;
 use sdl2::pixels::Color;
 use sdl2::rect::Rect;
+use sdl2::keyboard::Keycode;
 use std::fs::File;
 use std::io::Read;
 
@@ -12,6 +13,7 @@ use std::env;
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+const TICKS_PER_FRAME: usize = 10;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -50,15 +52,32 @@ fn main() {
     'gameloop: loop {
         for evt in event_pump.poll_iter() {     // Checks if any events have been triggered
             match evt {
-                Event::Quit{..} => {                   // Handles Quit
+                Event::Quit{..} | Event::KeyDown {keycode: Some(Keycode::Escape), ..} => {      // Handles Quit
                     break 'gameloop;
+                },
+
+                Event::KeyDown{keycode: Some(key), ..} => {                            // Handles Keydown                                          
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, true);
+                    }
+                },
+
+                Event::KeyUp{keycode: Some(key), ..} => {                              // Handles Keyup
+                    if let Some(k) = key2btn(key) {
+                        chip8.keypress(k, false);
+                    }
                 },
                 _ => ()
             }
         }
 
         // Clock cycle
-        chip8.tick();
+        for _ in 0..TICKS_PER_FRAME {
+            chip8.tick();
+        }
+
+        chip8.tick_timers();
+        
         // Draw screen
         draw_screen(&chip8, &mut canvas);
     }
@@ -87,3 +106,25 @@ fn draw_screen(emu: &Emu, canvas: &mut Canvas<Window>) {
     }
     canvas.present();
 }
+
+fn key2btn(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode:: Num1 => Some(0x1),
+        Keycode:: Num2 => Some(0x2),
+        Keycode:: Num3 => Some(0x3),
+        Keycode:: Num4 => Some(0xC),
+        Keycode:: Q =>    Some(0x4),
+        Keycode:: W =>    Some(0x5),
+        Keycode:: E =>    Some(0x6),
+        Keycode:: R =>    Some(0xD),
+        Keycode:: A =>    Some(0x7),
+        Keycode:: S =>    Some(0x8),
+        Keycode:: D =>    Some(0x9),
+        Keycode:: F =>    Some(0xE),
+        Keycode:: Z =>    Some(0xA),
+        Keycode:: X =>    Some(0x0),
+        Keycode:: C =>    Some(0xB),
+        Keycode:: V =>    Some(0xF),
+        _ => None,
+    }
+}   
